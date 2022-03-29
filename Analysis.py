3916d18e709be0a93c1576a11ca00aa601a26f25
Dylan Hubble <Hubbledylan@gmail.com>
2022-03-29 20:09:49 -0300

    Added more comments

--- Analysis.py
+++ Analysis.py
@@ -21,7 +21,8 @@ FuelMultiplier = 0.4329
 FuelBase = 12.27
 FuelPrice = 1.42
 
-#Number of roads in the network
+#number of roads in the network
+#calculation is exponential so be careful with this
 Roads = 1000
 
 #takes a list of nodes in a given path
@@ -33,7 +34,7 @@ def PathCost(graph,path,weight):
         total += graph[path[i]][path[i+1]][weight]
     return total
 
-#Calculate the total length of the network
+#calculate the total length of the network
 def TotalLength(G):
     #total length of the network
     total = 0
@@ -41,11 +42,11 @@ def TotalLength(G):
         total += a['Length']
     return total
 
-#Calculate the total number of nodes in the network
+#calculate the total number of nodes in the network
 def TotalNodes(G):
     return len(G.nodes)
 
-#Adds text to the final report
+#adds text to the final report
 def Write(Report,x,y,Write,bold=False,fontsize=10):
     if bold:
         Report.setFont("Helvetica-Bold",fontsize)
@@ -60,18 +61,22 @@ Network.add_nodes_from([i for i in range(Roads)])
 
 #generate network
 #iterates through all nodes and randomly adds edges as long as the total conennections is less than 3, and the edge is not a self loop
-#this is because a intersection can never have more than 4 conenctions (in most cases)
+#this is because a intersection can never have more than 4 connections (in most cases)
+#cap the bottom 5 and top 5 to not get out of range error
 for i in range(5,Roads-5):    
     start,end = random.randint(-5,5) + i, random.randint(-5,5) + i
     if start == end or Network.degree[start]>=3 or Network.degree[end]>=3: continue
     
+    #pulled from known average distances and divided by 1000 to turn into kilometers
     Length = random.randint(200,16000)/1000
     RoadSpeed = random.choices(Speed,SpeedFrequency)[0]
     RoadGVW = random.choices(GVW,GVWFrequency)[0]
     Time = Length/RoadSpeed
+    #standard calulation used for determining full trip cost
+    #math works on a per road basis as well giving the total cost to traverse including drivers cost + fuel cost
     Cost = DrivingRate*Time + ((FuelMultiplier*RoadSpeed)+FuelBase)*Time*FuelPrice
     
-    #adds the new road to the network along with a colour repersenting GVW for plotting
+    #adds the new road to the network along with all of its attributes
     Network.add_edge(
         start,
         end,
@@ -82,13 +87,14 @@ for i in range(5,Roads-5):
         Cost = Cost,
     )
 
-#removes all unconnected nodes
+#removes all nodes with no connections
+#could be expanded to remove all disconnected graphs
 for i in range(Roads):
     if Network.degree[i]<1:
         Network.remove_node(i)
 
 
-#Sends trucks from every intersection to every other intersection with routing based on different factors
+#sends trucks from every intersection to every other intersection with routing based on different factors
 #Wrote it expanded to be easy to read
 #what is the cost,length,time optimizing by cost,length,time
 def OptimalRoutes(Network,Report,y):
@@ -123,8 +129,9 @@ def OptimalRoutes(Network,Report,y):
                 pass
     #add results to final report
     Write(Report,40,y, "simulated 1 way loads : "+str(count))
-    Write(Report,40,y-20,"Cost of Cost vs Time = " + str(100-(Cost[0]/Time[0])*100)[:5] + " % savings",bold=True)
+    Write(Report,40,y-20,"Cost of Cost vs Time = " + str("{:.1f}".format(100-(Cost[0]/Time[0])*100)) + " % savings",bold=True)
     Write(Report,40,y-40,"optimized                    Cost         Length         Time")
+    #all numbers formatted to 1 decimal place for alignment on report
     Write(Report,40,y-60,"Cost Optimized     = $" + str("{:.1f}".format(Cost[0])) + " , " + str("{:.1f}".format(Cost[1])) + "km, " + str("{:.1f}".format(Cost[2]))+"hrs")
     Write(Report,40,y-80,"Length Optimized = $" + str("{:.1f}".format(Length[0])) + " , " + str("{:.1f}".format(Length[1])) + "km, " + str("{:.1f}".format(Length[2]))+ "hrs")
     Write(Report,40,y-100,"Time Optimized    = $" + str("{:.1f}".format(Time[0])) + " , " + str("{:.1f}".format(Time[1])) + "km, " + str("{:.1f}".format(Time[2]))+"hrs")
@@ -134,16 +141,19 @@ def OptimalRoutes(Network,Report,y):
 
 
 
-#Determines the total Costs savings of finding a route for when a truck is empty and a different route for when it is loaded
+#determines the total Costs savings of finding a route for when a truck is empty and a different route for when it is loaded
 #compares that vs always going in and out assuming that a truck is loaded
 def OneWay(truckW,Network,Report,y):
     #makes a copy of the network with only roads where GVW > truckW
     removed = Network.copy()
     remove = []
+    #makes a list of all edges with gvw < truck weight
     for start,end,a in Network.edges(data=True):
         if Network.edges[start,end]['GVW']<truckW:
             remove.append((start,end))
+    #removes all elements in remove list
     removed.remove_edges_from(remove)
+    
 
     #loops through intersection to every intersection and sums the cost to go there and back taking different routes
     countloaded = 0
@@ -158,17 +168,19 @@ def OneWay(truckW,Network,Report,y):
                 totalCostunloaded += PathCost(Network,nx.shortest_path(Network, j, i, weight='Cost'),'Cost')
                 countloaded += 1
             except:
+                #again there is expected to be some routes that are impossible
+                #especially with the reduced network size many "no path found"s are expected
                 pass
-    #Total savings calculated as total cost to take empty and loaded vs always going loaded route
+    #total savings calculated as total cost to take empty and loaded vs always going loaded route
     savings = 100-(totalCostloaded+totalCostunloaded)/(totalCostloaded*2)*100
 
-    #Adds data to final report
+    #adds data to final report
     Write(Report,40,y,"Tested truck size = "+str(truckW))
     Write(Report,170,y,"Total # of trips for 1 way routing ="+str(countloaded))
     Write(Report,350,y,"One way route Total Cost Savings = "  + str("{:.3f}".format(savings)) + " %",bold=True)
 
 
-#Generate Images for Report
+#generate images for report
 def GraphBarChart(x,y,xlabel,ylabel,FileName):
     plt.figure(figsize=(8,6))
     plt.bar(x,y)
@@ -180,7 +192,7 @@ def GraphBarChart(x,y,xlabel,ylabel,FileName):
 GraphBarChart(Speed,SpeedFrequency,"Speed (km/h)","Fraction of roads","Speed.png")
 GraphBarChart(GVW,GVWFrequency,"GVW (t)","Fraction of roads","GVW.png")
 
-#Generates and saves an image of the network 
+#generates and saves an image of the network 
 pos = nx.spring_layout(Network,weight='Length')
 nx.draw(Network, pos=pos, with_labels = True)
 plt.savefig("net.png")
@@ -202,11 +214,13 @@ Write(Report,10,800,"Road Network Simulation",bold=True,fontsize=24)
 Write(Report,350,740,"Road network details")
 Write(Report,350,720,"Total length of network = "+str("{:.1f}".format(TotalLength(Network)))+"km")
 Write(Report,350,700,"Total number of nodes = "+str(TotalNodes(Network)))
+#other details are written in OptimalRoutes around average trip details
 
-
+#images generated and saved above
 Report.drawImage("GVW.png",10,290,width=250,height=200)
 Report.drawImage("Speed.png",280,290,width=250,height=200)
 
+#fuel and sav come are excel charts that were generated from repeated runs to graph average results
 Report.drawImage("net.png",350,10,width=280,height=280)
 Report.drawImage("fuel.png",40,30,width=150,height=200)
 Report.drawImage("sav.png",200,30,width=150,height=200)
